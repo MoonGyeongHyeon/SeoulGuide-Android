457251017d07dd80ed7feb7a1da17d5d4eb74f44
MKH <kirioka@naver.com>
2017-02-09 16:53:20 +0900

    태그 프래그먼트 레이아웃 완성. * 카테고리 태그 - 전체, 유원지, 박물관 등을 선택하는 태그   서브 카테고리 태그 - 분위기, 연령, 지역 등을 선택하는 태그 1.  f_tag.xml 파일 레이아웃 구조는 다음과 같다.   1-1. 최상단의 카테고리 태그 4개를 갖는 LinearLayout이 존재한다.   1-2. 그 아래부터는 보여지는 뷰가 나뉜다.     1-2-1. 선택된 태그들로부터 선별된 여행지 목록이 보여질, RecyclerView가 존재하는 화면이다.     1-2-2. 카테고리와 서브 카테고리를 더 상세히 선택할 수 있는 화면이다. 나머지 카테고리 태그가 보여지는 LinearLayout과               그 아래로는 서브 카테고리 종류를 보여주는 TextView, 서브 카테고리를 나열하는 GridLayout과 선택할 수 있도록               도와주는 CheckBox가 반복되는 구조로 존재한다.   1-3. 최하단에는 선택된 태그를 적용시키는 확인 버튼과 체크박스를 초기화하는 버튼이 존재한다.
    2. 카테고리 태그와 확장/축소 뷰, 서브 카테고리 종류에 쓰일 이미지들을 추가.
    
    3. 카테고리 태그와 확장/축소 뷰가 선택됐을 때와 선택되지 않았을 때를 구분하기 위해 xml 파일 추가.
      3-1. 카테고리 태그는 상태에 따라 tag_category.xml 파일에 의해 배경 색이 바뀐다.
      3-2. 확장/축소 뷰는 상태에 따라 tag_more_ellipsis.xml 파일에 의해 이미지가 바뀐다.
    
    4. 태그의 CheckBox 고유 이미지를 삭제.
      4-1. 태그는 모두 CheckBox View가 사용됐는데 체크 여부를 확인하는 고유 이미지(사각형)가
            필요 없는 부분(카테고리 태그)은 고유 이미지가 보이지 않도록 TagFragment.java에 추가.

--- app/src/main/java/test/superdroid/com/seoulguide/Tag/TagFragment.java
+++ app/src/main/java/test/superdroid/com/seoulguide/Tag/TagFragment.java
@@ -1,19 +1,80 @@
 package test.superdroid.com.seoulguide.Tag;
 
+import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import test.superdroid.com.seoulguide.R;
 
 public class TagFragment extends Fragment {
+    private RecyclerView mSightRecyclerView;
+    private LinearLayout mMoreLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.f_tag, container, false);
+
+        mSightRecyclerView = (RecyclerView) layout.findViewById(R.id.tagSightRecyclerView);
+        mMoreLayout = (LinearLayout) layout.findViewById(R.id.tagMoreLayout);
+
+        initCheckBox(layout);
+
         return layout;
+    }
+
+    private void initCheckBox(View layout) {
+        CheckBox checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryAllCheckBox);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox1);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox2);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox3);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagMoreCheckBox);
+        checkBox.setButtonDrawable(new StateListDrawable());
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    mMoreLayout.setVisibility(View.VISIBLE);
+                    mSightRecyclerView.setVisibility(View.GONE);
+                } else {
+                    mMoreLayout.setVisibility(View.GONE);
+                    mSightRecyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox4);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox5);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox6);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox7);
+        checkBox.setButtonDrawable(new StateListDrawable());
+
+        checkBox = (CheckBox) layout.findViewById(R.id.tagCategoryCheckBox8);
+        checkBox.setButtonDrawable(new StateListDrawable());
     }
 }
